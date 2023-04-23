62d02a0473b2377dc10447439dfb6b60ac1e451f
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-24 01:44:47 +0200

    realtions are ready

---
+++ src/api/badge/content-types/badge/lifecycles.ts
@@ -0,0 +1,14 @@
+// export default {
+// beforeCreate(event: any) {
+// const { data, where, select, populate } = event.params;
+// console.log("DATA::", data);
+// console.log("EVENT::", event);
+// },
+//  beforeDelete(event: any) {
+// const { data, where, select, populate } = event.params;
+// },
+// afterCreate(event: any) {
+// const { result, params } = event;
+// do something to the result;
+// },
+// };
--- src/api/badge/controllers/badge.ts
+++ src/api/badge/controllers/badge.ts
@@ -2,6 +2,17 @@
  * badge controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::badge.badge');
+export default factories.createCoreController("api::badge.badge");
+// export default factories.createCoreController("api::badge.badge", {
+//   async find(ctx) {
+//     console.log("CONTEXT:", ctx);
+//     const idOfUserSendingRequest = ctx.state.user.id;
+//     const badges = await strapi.services["api::badge.badge"].find({
+//       ...ctx.query,
+//       //   user: ctx.state.user.id,
+//     });
+//     return badges.map((badge: any) => badge);
+//   },
+// });
