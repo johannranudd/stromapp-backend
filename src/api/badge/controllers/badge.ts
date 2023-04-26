50eb91aaf1d3aea4bde2593b3d21ea9774eb71b5
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-26 09:27:08 +0200

    adds funcitonal backend

--- src/api/badge/controllers/badge.ts
+++ src/api/badge/controllers/badge.ts
@@ -5,15 +5,3 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController("api::badge.badge");
-// export default factories.createCoreController("api::badge.badge", {
-//   async find(ctx) {
-//     console.log("CONTEXT:", ctx);
-//     const idOfUserSendingRequest = ctx.state.user.id;
-//     const badges = await strapi.services["api::badge.badge"].find({
-//       ...ctx.query,
-//       //   user: ctx.state.user.id,
-//     });
-//     return badges.map((badge: any) => badge);
-//   },
-// });
-//
